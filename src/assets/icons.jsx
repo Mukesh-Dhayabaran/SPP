9ea8dcd52d7ba33364672aa7ce41ff19b1c75d24
mukeshdhayabaran@gmail.com <mukeshdhayabaran@gmail.com>
2025-08-21 14:47:31 +0530

    3 Pages merged Successfully

--- src/assets/icons.jsx
+++ src/assets/icons.jsx
@@ -1,7 +1,6 @@
-import PersonWithChair from '../../assets/person';
 import PersonIcon from "@mui/icons-material/Person";
 import EmailIcon from "@mui/icons-material/Email";
 import LockIcon from "@mui/icons-material/Lock";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-export {PersonWithChair,PersonIcon,EmailIcon,LockIcon,VisibilityIcon,VisibilityOffIcon};
+export {PersonIcon,EmailIcon,LockIcon,VisibilityIcon,VisibilityOffIcon};
